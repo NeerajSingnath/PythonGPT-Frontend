5a3ac634ac9c827ae524a8506707d67a6975659b
Neeraj Singnath <Neerajs604086@gmail.com>
2026-09-09 01:02:23 +0530

    pythonGPT frontend init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-zinc-950">
+      <h1 className="text-4xl font-semibold text-white">
+        PythonGPT
+      </h1>
+    </div>
+  );
+}
+
+export default App;
